211e93165aaa27effad196a0da5249ac9fae7848
Ody <melodymonyok@gmail.com>
2021-08-26 12:56:14 -0500

    Added event listener for when app connects to internet again.

--- public/db.js
+++ public/db.js
@@ -2,3 +2,26 @@ let db;
 
 // Here we create a new db request for our "budgetTracker" database.
 const request = indexedDB.open("budgetTracker", 1);
+
+request.onupgradeneeded = (e) => {
+
+};
+
+request.onerror = (e) => {
+
+}
+
+function checkDatabase() {
+
+};
+
+request.onsuccess = (e) => {
+
+};
+
+const saveRecord = (record) => {
+
+};
+
+// This event checks if the app comes back online.
+window.addEventListener("online", checkDatabase);
