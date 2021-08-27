eb9c445a97b221b98b90e5a8d6e56ce6c792b0d9
Ody <melodymonyok@gmail.com>
2021-08-26 21:10:52 -0500

    Added data to save records to indexedDB.

--- public/db.js
+++ public/db.js
@@ -3,6 +3,7 @@ let db;
 // Here we create a new db request for our "budgetTracker" database.
 const request = indexedDB.open("budgetTracker", 1);
 
+// This handles 
 request.onupgradeneeded = (e) => {
   console.log("Upgrading IndexDB");
 
@@ -13,7 +14,7 @@ request.onupgradeneeded = (e) => {
 
   db = e.target.result;
 
-  // what does autoIncrement possibly do here?
+  // This creates a new object Store if one does not already exist.
   if (db.objectStoreNames.length === 0) {
     db.createObjectStore("budgetStore", { autoIncrement: true });
   }
@@ -29,11 +30,28 @@ function checkDatabase() {
 };
 
 request.onsuccess = (e) => {
+  console.log("Success!");
+  db = e.target.result;
+
+  // After each transaction it will check if you are online before reading from the DB.
+  if (navigator.onLine) {
+    console.log("Connected to the interwebs again!");
+    checkDatabase();
+  }
 
 };
 
 const saveRecord = (record) => {
+  console.log('Current record saved.');
 
+  // Gives readwrite access to the created transaction for the DB.
+  const transaction = db.transaction(["budgetStore"], "readwrite");
+
+  // Gives access from the transaction to the object store.
+  const store = transaction.objectStore("budgetStore");
+
+  // Here we add a record to our store.
+  store.add(record);
 };
 
 // This event checks if the app comes back online.
